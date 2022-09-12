2eccbe0a14162d320098aac80df7bb23624db8f8
Stephen Fong <fongchinghinunsw@gmail.com>
2022-09-12 18:36:37 +0800

    Add verify config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -16,6 +17,7 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 
 const ETHEREUM_NODE_API_KEY = process.env.ETHEREUM_NODE_API_KEY;
 const WALLET_PRIVATE_KEY = process.env.WALLET_PRIVATE_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
@@ -33,5 +35,8 @@ module.exports = {
       url: `https://goerli.infura.io/v3/${ETHEREUM_NODE_API_KEY}`,
       accounts: [WALLET_PRIVATE_KEY],
     },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
   },
 };
